a58cbfc22f8a4664670537d13b4061a27d82c16a
Gage Pickle <gagepickle@yahoo.com>
2020-11-19 19:01:45 -0600

    Add example of async await

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import { promises as fs } from "fs";
 
 console.log("before contents");
 
-fs.readFile(__filename)
-  .then((contents) => contents)
-  .then((contents2Write) => fs.writeFile("output.txt", contents2Write));
+// Async immediately invoked function expression (ife)
+// Specifying 'async' in front of the fxn wraps this in a promise sort of like .then
+(async () => {
+  const contents = await fs.readFile(__filename, "utf-8");
+
+  fs.writeFile("output.txt", contents).then(() => {
+    console.log("success!");
+  });
+})();
 
 console.log("after contents");
